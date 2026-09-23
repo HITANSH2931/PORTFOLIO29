189ee05f298cc727e25f0db31dd67aeecd72df05
hitansh293105 <hitanshjoshi293105@gmail.com>
2026-09-23 15:03:35 +0530

    changes

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -10,14 +10,15 @@ const About = () => {
       <img src={image} className='w-lg h-72 rounded-lg bg-linear-to-r from-[#1cd8d2] via-[#00bf8f] to-[#302b63]'/>
         <div className='flex flex-col gap-5'>
           <h1 className='bg-linear-to-r from-[#1cd8d2] via-[#00bf8f] to-[#302b63] bg-clip-text text-transparent text-5xl font-bold'>Hitansh Joshi</h1>
-          <h2 className='text-white  font-semibold text-2xl'>Full Stack Developer</h2>
-          <p className='text-gray-300 text-[15px] '>Java Full-Stack Developer with expertise in Spring Boot, Microservices, Docker, and RESTful services. Skilled in building scalable, production-ready applications and deploying containerized solutions.</p>
-           
+          <h2 className='text-white  font-semibold text-2xl'>Front End Developer</h2>
+          <p className='text-gray-300 text-[15px]'>
+           Frontend Developer specializing in React.js, Redux, and Tailwind CSS, backed by strong expertise in Spring Boot, Microservices, and Docker. Skilled in building dynamic user interfaces and integrating secure, production-ready backend services for robust applications.
+          </p> 
            <div className='flex gap-5'>
            <div className=' border border-solid border-gray-800 py-5 px-10 rounded-lg flex flex-col gap-1.5'>
 
             <div className='text-gray-400 text-[14px]'>Speciality</div>
-            <div className='text-white font-semibold'>Full Stack</div>
+            <div className='text-white font-semibold'>FrontEnd</div>
               
            </div>
            <div className='text-gray-400 border border-solid border-gray-800 py-5 px-10 rounded-lg gap-1.5'>
@@ -33,7 +34,7 @@ const About = () => {
 
        <div className='mt-20 flex flex-col gap-3.5'>
         <h1 className='font-bold text-white text-3xl'>About me</h1>
-        <p className='text-gray-300 text-[15px]'>I am a Java Full Stack Developer from Dehradun, Uttarakhand, with a strong interest in building efficient, high-performance web applications. I specialize in creating end-to-end solutions and enjoy working with modern technologies.</p>
+        <p className='text-gray-300 text-[15px]'>I am a Front End Developer from Dehradun, Uttarakhand, with a strong interest in building efficient, high-performance web applications. I specialize in creating end-to-end solutions and enjoy working with modern technologies.</p>
       <p className='text-gray-300 text-[15px]'>Along with development, I love solving DSA problems and enjoy tackling challenging algorithms that sharpen my problem-solving skills.</p>
        </div>
     </section>
